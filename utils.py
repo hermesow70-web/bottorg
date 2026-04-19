98d9dded204820f9eea0203f24f31ea9f8012e08
hermesow70-web <hermesow70@gmail.com>
2026-04-20 00:03:22 +0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,19 +1,18 @@
 import asyncio
 from config import ADMIN_IDS
-from database import load_users, load_banned
+from database import load_users, is_banned
 
-async def notify_admins(bot, text, reply_markup=None):
+async def notify_admins(bot, text):
     for admin_id in ADMIN_IDS:
         try:
-            await bot.send_message(admin_id, text, reply_markup=reply_markup, parse_mode="Markdown")
+            await bot.send_message(admin_id, text, parse_mode="Markdown")
         except:
             pass
 
 async def notify_all_users(bot, text):
     users = load_users()
-    banned = load_banned()
     for user in users:
-        if user["id"] in banned:
+        if is_banned(user["id"]):
             continue
         try:
             await bot.send_message(user["id"], text, parse_mode="Markdown")
