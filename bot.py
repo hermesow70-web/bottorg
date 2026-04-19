4e525edf63c4deda0c2f6c243ecbcd865cdb4524
hermesow70-web <hermesow70@gmail.com>
2026-04-20 02:07:54 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,20 +2,16 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from config import TOKEN
 from user import user_router
-from withdraw import withdraw_router
-from review import review_router
 from admin import admin_router
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 dp.include_router(user_router)
-dp.include_router(withdraw_router)
-dp.include_router(review_router)
 dp.include_router(admin_router)
 
 async def main():
-    print("🤖 БОТ ЗАПУЩЕН!")
+    print("🤖 Бот запущен!")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
