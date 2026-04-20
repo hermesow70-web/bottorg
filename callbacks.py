86d78501a75c5ae6e813690e30a14f9fe696da4d
hermesow70-web <hermesow70@gmail.com>
2026-04-20 14:18:28 +0400

    Delete callbacks.py

--- callbacks.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Router
-from user import user_router
-from admin import admin_router
-
-callback_router = Router()
-
-# Все callback'и уже обрабатываются в user.py и admin.py
-# Этот файл нужен для импорта в bot.py
