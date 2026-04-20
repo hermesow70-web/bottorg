3e2dc7c8fc03f7682c0d7302c257fd28c4618830
hermesow70-web <hermesow70@gmail.com>
2026-04-20 15:20:06 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,18 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from config import TOKEN
-from handlers import router
+from user_profile import profile_router
+from user_withdraw import withdraw_router
+from user_review import review_router
+from admin import admin_router
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
-dp.include_router(router)
+
+dp.include_router(profile_router)
+dp.include_router(withdraw_router)
+dp.include_router(review_router)
+dp.include_router(admin_router)
 
 async def main():
     print("🤖 Бот запущен!")
